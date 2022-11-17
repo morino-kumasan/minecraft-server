d057c11400f7f9af7c490e0754265a7f7f5faca0
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2022-11-17 12:57:42 +0900

    eulaの処理追加

--- docker/minecraft/entrypoint.sh
+++ docker/minecraft/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -eu
 
+# world dir
 world="/minecraft/worlds/${WORLD_NAME}"
 mkdir -p "${world}"
 cd "${world}"
 
+# agree minecraft eula
+if [[ -f eula.txt ]]; then
+  grep -q '^eula=false$' eula.txt && sed -i -e "s/^eula=false$/eula=${EULA}/g" eula.txt
+fi
+
+# start server
 exec java "-Xmx${HEAP_MAX}" "-Xms${HEAP_INIT}" -jar /minecraft/paper.jar nogui
