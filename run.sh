fd28cefa3a241b94ca30763c04c58accbbdb5c1e
morino-kumasan <74145127+morino-kumasan@users.noreply.github.com>
2022-12-17 05:00:05 +0900

    docker-composeやめる

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eu
+
+export MSYS_NO_PATHCONV=1
+
+(
+  cd "$(dirname "$0")"
+  #source ./.env
+
+  WORLD_NAME=${WORLD_NAME:-default_world}
+
+  docker build \
+    -t minecraft \
+    --build-arg MINECRAFT_VERSION=1.19.2 \
+    --build-arg PAPER_MINOR_VERSION=271 \
+    ./docker/minecraft
+
+  docker run -itd \
+    --mount "type=bind,src=$(pwd)/worlds,dst=/minecraft/worlds" \
+    -e EULA=true \
+    -e "WORLD_NAME=${WORLD_NAME}" \
+    -e "HEAP_INIT=${HEAP_INIT:-1024M}" \
+    -e "HEAP_MAX=${HEAP_MAX:-1024M}" \
+    -p ${LISTEN_PORT:-25565}:25565 \
+    --restart unless-stopped \
+    --name "minecraft-server-${WORLD_NAME}" \
+    minecraft
+)
---
+++ stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+export MSYS_NO_PATHCONV=1
+
+(
+  cd "$(dirname "$0")"
+  #source ./.env
+
+  docker stop "minecraft-server-${WORLD_NAME:-default_world}"
+)
